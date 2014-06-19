31c5d39e2f3178d0991b3f4a5af2a81a87271ae8
Rogério Brito <rbrito@ime.usp.br>
2014-06-19 06:36:10 -0300

    chap01: ex05: Add exercise with input and output of integers.

---
+++ chap01/ex05.scala
@@ -0,0 +1,18 @@
+object ex05 {
+  def main(args: Array[String]) = {
+    var i = 1 // next day to read
+    var max = -1
+    var maxday = 0
+
+    while (i <= 31) {
+      var x = Console.readInt()
+      if (x > max) {
+        max = x
+        maxday = i
+      }
+      i += 1
+    }
+    println(max)
+    println(maxday)
+  }
+}
