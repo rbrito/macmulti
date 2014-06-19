f5e06711063c969e8f36fcac0715292c6680ee28
Rogério Brito <rbrito@ime.usp.br>
2014-06-19 06:04:40 -0300

    chap01: ex02: Add exercise with input and output of integers.

---
+++ chap01/ex02.scala
@@ -0,0 +1,14 @@
+object ex02 {
+  def main(args: Array[String]) = {
+    var n = Console.readInt() // we avoid using n choose 2
+    var i = 0
+    var sum = 0
+
+    while (i < n) {
+      sum += i
+      i += 1
+    }
+
+    println(sum)
+  }
+}
