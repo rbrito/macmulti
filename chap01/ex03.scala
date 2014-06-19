ac7953c463d82b237747025b4a0ded55264c583c
Rogério Brito <rbrito@ime.usp.br>
2014-06-19 06:10:15 -0300

    chap01: ex03: Add exercise with input and output of integers.

---
+++ chap01/ex03.scala
@@ -0,0 +1,11 @@
+object ex03 {
+  def main(args: Array[String]) = {
+    var n = Console.readInt() // we avoid using n choose 2
+    var printed = 0 // how many already printed
+
+    while (printed < n) {
+      println(2 * printed + 1)
+      printed += 1
+    }
+  }
+}
