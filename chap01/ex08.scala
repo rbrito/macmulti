aa4ccd4f2e820fb3f9028a4fc86b613d11c2bc3b
Rogério Brito <rbrito@ime.usp.br>
2014-06-21 01:28:15 -0300

    chap01: ex08: Add exercise with computation of factorials.

---
+++ chap01/ex08.scala
@@ -0,0 +1,13 @@
+object ex08 {
+  def main(args: Array[String]) = {
+    var n = Console.readInt()
+    var res = 1 // the partial result of n! so far
+    var i = 1
+
+    while (i <= n) {
+      res = res * i
+      i += 1
+    }
+    println(res)
+  }
+}
