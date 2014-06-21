8f5b1f2d68e11f7a61517de14f9e84867adea4f2
Rogério Brito <rbrito@ime.usp.br>
2014-06-20 23:23:57 -0300

    chap01: ex07: Add exercise with input and output of integers.

---
+++ chap01/ex07.scala
@@ -0,0 +1,15 @@
+object ex07 {
+  def main(args: Array[String]) = {
+    var n = Console.readInt() // how many integers to read
+    var sum = 0 // the sum of the even numbers read
+    var i = 0 // how many integers read
+
+    while (i < n) {
+      var num = Console.readInt()
+      if (num % 2 == 0)
+        sum = sum + num
+      i += 1
+    }
+    println(sum)
+  }
+}
