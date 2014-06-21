d07795bf659d54efb0cbe71edb4c66300e34b728
Rogério Brito <rbrito@ime.usp.br>
2014-06-21 01:37:18 -0300

    chap01: ex10: Add exercise with input and output of integers.

---
+++ chap01/ex10.scala
@@ -0,0 +1,16 @@
+object ex10 {
+  def main(args: Array[String]) = {
+    var n = Console.readInt()
+
+    var i = 0
+
+    while (i * (i+1) * (i+2) < n)
+      i += 1
+
+    if (i * (i+1) * (i+2) == n)
+      println(n + " is such product.")
+    else
+      println(n + " is not such product.")
+
+  }
+}
