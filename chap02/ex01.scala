d3b3dec407b9c9f8e21a1e1405c43a49c0f42667
Rogério Brito <rbrito@ime.usp.br>
2014-06-24 23:32:27 -0300

    chap02: ex01: Add exercise with nested loops.

---
+++ chap02/ex01.scala
@@ -0,0 +1,21 @@
+object ex01 {
+  def main(args: Array[String]) = {
+    var n = Console.readInt() // number of sequences
+    var i = 0 // for each sequence
+
+    while (i < n) {
+      var sum = 0
+      var num = 0
+
+      do {
+        num = Console.readInt()
+        if (num % 2 == 0)
+          sum += num
+      } while (num != 0)
+
+      println(sum)
+
+      i += 1
+    }
+  }
+}
